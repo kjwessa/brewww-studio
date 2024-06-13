874797d0078027b26d1329709241ddd27a4461ee
Kevin Wessa <kjwessa@gmail.com>
2024-06-13 11:49:49 -0400

    feat(ui): add services section to temp home page

---
+++ app/dev/components/HomeServices.tsx
@@ -0,0 +1,150 @@
+export function HomeServices() {
+  return (
+    <>
+      <div>
+        <div
+          className="relative content-stretch items-start justify-start bg-white px-24 py-36 font-light text-neutral-900"
+          id="div-1"
+        >
+          <div
+            className="relative m-auto flex w-full max-w-[62.50rem] flex-grow auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto_auto] flex-col items-stretch justify-center self-start"
+            id="div-2"
+          >
+            <div
+              className="relative grid auto-cols-fr grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr] grid-rows-[auto_auto_auto_auto_auto_auto] gap-4"
+              id="div-3"
+            >
+              <div
+                className="col-span-4 row-span-1 self-end overflow-hidden font-bold uppercase"
+                id="div-4"
+              >
+                <div className="flex flex-col items-start">
+                  <div className="pb-5">
+                    <span className="text-orange-400">/</span> What we do (and
+                    Do Well)
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-6 row-span-2 overflow-hidden text-[3.63rem] leading-none">
+                <div className="flex flex-col items-start">
+                  <h2 className="min-h-[0vw]">
+                    Brewww is a creative studio that finds the places where{" "}
+                    <strong className="font-extrabold">needs</strong>,{" "}
+                    <strong className="font-extrabold">stories</strong>, and{" "}
+                    <strong className="font-extrabold">technology</strong>{" "}
+                    overlap.
+                  </h2>
+                </div>
+              </div>
+              <div
+                className="col-start-1 col-end-3 row-start-5 row-end-7 overflow-hidden"
+                id="div-5"
+              >
+                <div className="flex flex-col items-start">
+                  <h4 className="min-h-[0vw] text-lg">Branding</h4>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/brand-strategy"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-6">
+                      Brand Strategy
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/brand-identity"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-7">
+                      Brand Identity
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/brand-content"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-8">
+                      Brand Content
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                </div>
+              </div>
+              <div
+                className="col-start-3 col-end-6 row-start-5 row-end-7 overflow-hidden"
+                id="div-9"
+              >
+                <div className="flex flex-col items-start">
+                  <h4 className="min-h-[0vw] text-lg">Web</h4>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/web-design-and-ux"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-10">
+                      Web Design & UX
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/web-development"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-11">
+                      Web Development
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/seo-and-search"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-12">
+                      SEO & Search
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                </div>
+              </div>
+              <div
+                className="col-start-5 col-end-6 row-end-7 overflow-hidden"
+                id="div-13"
+              >
+                <div className="flex flex-col items-start">
+                  <h4 className="min-h-[0vw] text-lg">Creative</h4>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/graphic-design"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-14">
+                      Graphic Design
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/photography"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-15">
+                      Photography
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                  <a
+                    className="relative mb-1 inline-block max-w-full font-bold text-white"
+                    href="/capabilities/social-media-management"
+                  >
+                    <div className="uppercase text-neutral-900" id="div-16">
+                      Social Media
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 w-1/5 bg-neutral-900" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
