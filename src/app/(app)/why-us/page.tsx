5685a8d371ecdaab87301d3652df5acfa8200b54
Kevin Wessa <kjwessa@gmail.com>
2024-06-19 15:11:53 -0400

    feat(ui): add initial reasons section on why us page

--- src/app/(app)/why-us/page.tsx
+++ src/app/(app)/why-us/page.tsx
@@ -1,8 +1,10 @@
 import { WhyHero } from "./WhyHero";
+import { WhyHeroNumbers } from "./WhyHeroNumbers";
 export default function Page() {
   return (
     <main>
       <WhyHero />
+      <WhyHeroNumbers />
     </main>
   );
 }
