337db6e670195fcd5c29c8424a6d8b7a19cba783
Kevin Wessa <kjwessa@gmail.com>
2024-06-25 10:04:00 -0400

    feat(ui): add initial PlayHero component to new play page

---
+++ src/app/(app)/play/PlayHero.tsx
@@ -0,0 +1,18 @@
+export function PlayHero() {
+  return (
+    <>
+      <div>
+        <div className="max-w-[82.50rem] text-xl text-neutral-800 min-[480px]:max-w-[84.38rem] min-[720px]:max-w-[86.88rem] min-[720px]:pl-12 min-[720px]:pr-12 min-[1080px]:max-w-[89.38rem] min-[1080px]:pl-16 min-[1080px]:pr-16">
+          <h1 className="mb-1 mt-28 text-[2.88rem] font-light leading-none text-white">
+            Brewww Playground
+          </h1>
+          <p className="mb-24 max-w-[67.50rem] text-[2.88rem] leading-none text-neutral-500">
+            A playground where the artisans at Brewww share internal projects
+            that they are playing around with. From the ridiculous to the
+            somewhat useful, we hone skillsets through imagination and fun.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
