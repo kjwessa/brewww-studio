ccc87aadf48a3c7265433ae7a8c693814d30c52b
Kevin Wessa <kjwessa@gmail.com>
2024-07-01 14:02:58 -0400

    chore(data): update blog post card images

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -20,22 +20,22 @@ export function BlogCard({
 }: BlogCardProps) {
   return (
     <div>
-      <div className="relative h-0 w-full pb-[66%]">
-        <Image src={image} alt={alt} layout="fill" objectFit="cover" />
-      </div>
+      <Link href={`think/${slug}`}>
+        <div className="relative h-0 w-full pb-[66%]">
+          <Image
+            src={image}
+            alt={alt}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
 
-      <div className="flex-1">
-        <h3 className="my-2 text-xl font-bold">{title}</h3>
-        <p className="mb-4 text-base text-gray-800">{description}</p>
-        <p className="text-sm text-gray-500">
-          {new Date(`${publishedAt}`).toLocaleDateString()}
-        </p>
-      </div>
-      <Link
-        href={`think/${slug}`}
-        className="text-indigo-600 hover:text-indigo-900"
-      >
-        Read More
+        <div className="flex-1">
+          <h3 className="my-2 text-xl font-bold">{title}</h3>
+          <p className="mb-4 text-base text-gray-800">{description}</p>
+        </div>
       </Link>
     </div>
   );
