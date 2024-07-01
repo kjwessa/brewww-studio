b0d6e4fd0ede3ff66c3f48966abd6dd3460d95ed
Kevin Wessa <kjwessa@gmail.com>
2024-07-01 10:09:11 -0400

    chore: add untracked files to commit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,16 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Footer } from "../components/Footer";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const DMSans = localFont({
+  variable: "--font-dm-sans",
+  display: "swap",
+  src: [
+    { path: "../fonts/DMSans.ttf", weight: "100 400 900", style: "normal" },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Brewww Studio",
@@ -14,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${DMSans.variable}`}>
       <body className="bg-gray-950 text-gray-50 antialiased">
         <main className="flex min-h-svh flex-col">{children}</main>
         <Footer />
