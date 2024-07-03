525f454be6d039ed5845edeb3af7191a66cc9f38
Kevin Wessa <kjwessa@gmail.com>
2024-07-03 11:14:51 -0400

    feat(ui): add initial ServicesTestimonial section to Services page

---
+++ src/app/(app)/services/ServicesTestimonial.tsx
@@ -0,0 +1,33 @@
+export function ServicesTestimonial() {
+  return (
+    <>
+      <section className="border-t-2 border-solid border-stone-950 bg-white px-16 py-32 text-center text-stone-950">
+        <div>
+          <div>
+            <h2 className="text-4xl font-bold uppercase">
+              &quot;We have been getting more phone calls and emails than ever
+              before.&quot;
+            </h2>
+
+            <p className="p-8">
+              &quot;Kevin designed a website for our Storage facility and we
+              couldn&apos;t be happier with the ease of navigation and the
+              entire process. We have been getting more phone calls and emails
+              than ever before. Kevin was great to work with - he was very
+              responsive and professional. He brought a lot of expertise and
+              creative ideas to help us execute our vision. I absolutely love
+              our website! I highly recommend Brewww Studio for all your web
+              development needs.&quot;
+            </p>
+
+            <p className="text-base">
+              Mandy Robertson
+              <br />
+              Azle Storage
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/(app)/services/page.tsx
+++ src/app/(app)/services/page.tsx
@@ -1,11 +1,13 @@
 import { ServicesHero } from "./ServicesHero";
 import { ServicesIntro } from "./ServicesIntro";
+import { ServicesTestimonial } from "./ServicesTestimonial";
 
 export default function Page() {
   return (
     <>
       <ServicesHero />
       <ServicesIntro />
+      <ServicesTestimonial />
     </>
   );
 }
