a10420bc7446dd08467c6a266d82cf7b70118065
Kevin Wessa <kjwessa@gmail.com>
2024-07-03 17:31:06 -0400

    feat(ui): add initial intro section to individual Series page

--- src/app/(app)/services/[slug]/page.tsx
+++ src/app/(app)/services/[slug]/page.tsx
@@ -17,31 +17,53 @@ export default function ServicePage({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <section className="flex items-end bg-stone-950 px-16 pt-44 text-center text-white">
-        <div className="flex flex-wrap justify-center">
-          <h1 className="flex flex-col justify-center text-[5.13rem] leading-none">
-            <div className="table pr-1">
-              <div>
-                <h1>{service.pageTitle}</h1>
+      <div className="bg-stone-950 text-white">
+        <section className="flex items-end px-16 pt-44 text-center">
+          <div className="flex flex-wrap justify-center">
+            <h1 className="flex flex-col justify-center text-[5.13rem] leading-none">
+              <div className="table pr-1">
+                <div>
+                  <h1>{service.pageTitle}</h1>
+                </div>
+              </div>
+            </h1>
+
+            <div className="flex w-full justify-between pb-4 pt-16 text-4xl font-bold uppercase">
+              <h2>Service</h2>
+              <h2>Contact Us</h2>
+            </div>
+
+            <div className="relative flex h-0 w-full items-end pb-[66%]">
+              <Image
+                src={service.featuredImg}
+                alt="text"
+                fill
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          </div>
+        </section>
+        <section className="px-16 pt-20">
+          <div className="m-auto w-full max-w-[120.00rem]">
+            <div className="flex" id="div-1">
+              <div
+                className="flex w-[55%] flex-col pr-[calc(min(6vw,_115px)_*_2)] text-3xl font-bold uppercase"
+                id="div-2"
+              >
+                <h2 className="overflow-hidden">
+                  <div className="">
+                    <h2>{service.heroSection.headline}</h2>
+                  </div>
+                </h2>
+              </div>
+
+              <div className="flex w-[45%] flex-col" id="div-3">
+                <p>{service.heroSection.description}</p>
               </div>
             </div>
-          </h1>
-
-          <div className="flex w-full justify-between pb-4 pt-16 text-4xl font-bold uppercase">
-            <h2>Service</h2>
-            <h2>Contact Us</h2>
           </div>
-
-          <div className="relative flex h-0 w-full items-end pb-[66%]">
-            <Image
-              src={service.featuredImg}
-              alt="text"
-              fill
-              style={{ objectFit: "cover" }}
-            />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
