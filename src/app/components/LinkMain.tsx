4acdfe1314fd128e8fcd8acfc513a2dc9fab96a0
Kevin Wessa <kjwessa@gmail.com>
2024-07-11 10:12:09 -0400

    feat(LinkMain): add showArrow optional prop

--- src/app/components/LinkMain.tsx
+++ src/app/components/LinkMain.tsx
@@ -3,26 +3,29 @@ import Link from "next/link";
 interface LinkMainProps {
   text: string;
   href: string;
+  showArrow?: boolean;
 }
-export function LinkMain({ text, href }: LinkMainProps) {
+export function LinkMain({ text, href, showArrow = true }: LinkMainProps) {
   return (
     <>
-      <Link className="flex text-lg font-medium text-gray-950" href={href}>
+      <Link className="flex text-base font-medium text-gray-950" href={href}>
         <span className="flex cursor-pointer items-center">
           <span>{text}</span>
-          <svg
-            className="h-6 w-6 text-slate-300"
-            fill="rgb(187, 196, 210)"
-            preserveAspectRatio="none"
-            role="presentation"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.6075 11.8572L13.255 8.40897L14.1388 7.5L19 12.5L14.1388 17.5L13.255 16.591L16.6075 13.1428H5V11.8572H16.6075Z"
+          {showArrow && (
+            <svg
+              className="h-6 w-6 text-slate-300"
               fill="rgb(187, 196, 210)"
-            />
-          </svg>
+              preserveAspectRatio="none"
+              role="presentation"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.6075 11.8572L13.255 8.40897L14.1388 7.5L19 12.5L14.1388 17.5L13.255 16.591L16.6075 13.1428H5V11.8572H16.6075Z"
+                fill="rgb(187, 196, 210)"
+              />
+            </svg>
+          )}
         </span>
       </Link>
     </>
