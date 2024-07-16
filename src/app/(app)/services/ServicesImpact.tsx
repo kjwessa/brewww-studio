bd86c8f3617a7658d79daab9ce31e2d3bf3c5b56
Kevin Wessa <kjwessa@gmail.com>
2024-07-16 13:19:57 -0400

    refactor(impact): rebuild ServicesImpact with new ImpactCard components

--- src/app/(app)/services/ServicesImpact.tsx
+++ src/app/(app)/services/ServicesImpact.tsx
@@ -5,51 +5,29 @@ export function ServicesImpact() {
     <>
       <div className="bg-white text-stone-950">
         <div className="mx-auto w-full max-w-6xl px-8 py-12">
-          <div className="m-auto w-full max-w-[90.00rem]" id="div-1">
-            <div className="text-2xl font-medium">Impact & Results</div>
-            <div className="grid grid-cols-3 gap-8 pt-12">
-              <div className="">
-                <div className="flex items-start text-6xl">
-                  3<span className="ml-3 mt-6 text-xl">M</span>
-                </div>
-                <div className="mb-12 text-lg">
-                  Brand strategy and site development of The Merry Beggars led
-                  to 3 million downloads of their original audio entertainment
-                  podcast.
-                </div>
-
-                <a className="text-lg font-medium text-blue-700" href="">
-                  View Case Study
-                </a>
-              </div>
-              <div className="">
-                <div className="flex items-start text-6xl">
-                  10<span className="ml-3 mt-6 text-xl">X</span>
-                </div>
-                <div className="mb-12 text-lg">
-                  Brand strategy and development of Fem Catholic led to a grant
-                  for their initiative, ten times their original site
-                  investment.
-                </div>
-
-                <a className="text-lg font-medium text-blue-700" href="">
-                  View Case Study
-                </a>
-              </div>
-              <div className="">
-                <div className="flex items-start text-6xl">
-                  760<span className="ml-3 mt-6 text-xl">%</span>
-                </div>
-                <div className="mb-12 text-lg">
-                  Increase in online donations within 90 days after Joseph House
-                  site rebrand and website launch.
-                </div>
-
-                <a className="text-lg font-medium text-blue-700" href="">
-                  View Case Study
-                </a>
-              </div>
-            </div>
+          <div className="text-2xl font-medium">Impact & Results</div>
+          <div className="grid grid-cols-3 gap-8 pt-12">
+            <ImpactCard
+              number={3}
+              letter={"m"}
+              text={
+                "Brand strategy and site development of The Merry Beggars led to 3 million downloads of their original audio entertainment podcast."
+              }
+            />
+            <ImpactCard
+              number={10}
+              letter={"x"}
+              text={
+                " Brand strategy and development of Fem Catholic led to a grant for their initiative, ten times their original site investment."
+              }
+            />
+            <ImpactCard
+              number={760}
+              letter={"%"}
+              text={
+                "Increase in online donations within 90 days after Joseph House site rebrand and website launch."
+              }
+            />
           </div>
         </div>
       </div>
--- src/app/components/ImpactCard.tsx
+++ src/app/components/ImpactCard.tsx
@@ -2,9 +2,22 @@ interface ImpactCardProps {
   number: number;
   letter: string;
   text: string;
-  link: string;
+  link?: string;
 }
 
 export function ImpactCard({ number, letter, text, link }: ImpactCardProps) {
-  return <div></div>;
+  return (
+    <div className="">
+      <div className="flex items-start text-6xl">
+        {number}
+        <span className="ml-3 mt-6 text-xl">{letter}</span>
+      </div>
+      <div className="mb-12 text-lg">{text}</div>
+      {link && (
+        <a className="text-lg font-medium text-blue-700" href="">
+          View Case Study
+        </a>
+      )}
+    </div>
+  );
 }
