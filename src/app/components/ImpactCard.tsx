84ef3d50cb6f355e74779b7b34477680a8cefc43
Kevin Wessa <kjwessa@gmail.com>
2024-07-16 13:12:08 -0400

    feat(impact): separate ImpactCard into separate component and add types

--- src/app/components/ImpactCard.tsx
+++ src/app/components/ImpactCard.tsx
@@ -1,3 +1,10 @@
-export function ImpactCard({ number, letter, text, link }) {
+interface ImpactCardProps {
+  number: number;
+  letter: string;
+  text: string;
+  link: string;
+}
+
+export function ImpactCard({ number, letter, text, link }: ImpactCardProps) {
   return <div></div>;
 }
