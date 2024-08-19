16542d8d601f82346d82ab1aa0c45d01e36721f3
Kevin Wessa <kjwessa@gmail.com>
2024-08-18 22:05:52 -0400

    feat(cms): update Results collection fields

--- src/collections/Results.ts
+++ src/collections/Results.ts
@@ -6,8 +6,22 @@ export const Results: CollectionConfig = {
     {
       name: "name",
       type: "text",
-      label: "Name",
+      label: "Name (Internal)",
       required: true,
+    },
+    {
+      name: "client",
+      type: "relationship",
+      relationTo: "clients",
+      required: true,
+    },
+    { name: "number", type: "text", label: "Number", required: true },
+    { name: "support", type: "text", label: "Support", required: true },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Description",
+      required: false,
     },
   ],
 };
