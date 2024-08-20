0f0da6af7e333b5906effe2d8ddb9a96c7c0a517
Kevin Wessa <kjwessa@gmail.com>
2024-08-20 13:20:07 -0400

    feat(cms): add initial Playground collection to Payload

---
+++ src/collections/Playground.ts
@@ -0,0 +1,36 @@
+import type { CollectionConfig } from "payload";
+
+export const Playground: CollectionConfig = {
+  slug: "play",
+  versions: {
+    drafts: true,
+    maxPerDoc: 25,
+  },
+  admin: {
+    useAsTitle: "name",
+  },
+  labels: {
+    singular: "Playground",
+    plural: "Playgrounds",
+  },
+  fields: [
+    { name: "name", type: "text", label: "Name", required: true },
+    {
+      name: "slug",
+      type: "text",
+      label: "Slug",
+      required: false,
+      admin: { position: "sidebar" },
+    },
+    {
+      name: "thumbnail",
+      type: "upload",
+      label: "Thumbnail",
+      required: false,
+      relationTo: "media",
+      admin: {
+        description: "This image appears on the PlayCard thumbnail images.",
+      },
+    },
+  ],
+};
