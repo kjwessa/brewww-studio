8d5f204d0547e216ccb00d12beb38b29df31e805
Kevin Wessa <kjwessa@gmail.com>
2024-08-20 12:38:46 -0400

    feat(cms): update Client collection in Payload

--- src/collections/Clients.ts
+++ src/collections/Clients.ts
@@ -14,7 +14,49 @@ export const Clients: CollectionConfig = {
       type: "text",
       label: "Name",
       required: true,
-      admin: { description: "Add the client name here." },
+      admin: {
+        description: "Add the client name as it will appear around the site.",
+      },
+    },
+    {
+      name: "logoLight",
+      type: "upload",
+      relationTo: "media",
+      required: false,
+      label: "Logo Light",
+      admin: {
+        description:
+          "Add the light version of the logo that appears on dark backgrounds",
+      },
+    },
+    {
+      name: "logoDark",
+      type: "upload",
+      relationTo: "media",
+      required: false,
+      label: "Logo Dark",
+      admin: {
+        description:
+          "Add the dark version of the logo that appears on light backgrounds",
+      },
+    },
+    {
+      name: "city",
+      type: "text",
+      label: "City",
+      required: true,
+      admin: {
+        description: "The closest major city to the client.",
+      },
+    },
+    {
+      name: "state",
+      type: "text",
+      label: "State",
+      required: true,
+      admin: {
+        description: "The state where the client is based.",
+      },
     },
   ],
 };
