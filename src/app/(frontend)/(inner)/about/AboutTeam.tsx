c8927422b9a979a9fb2a0ab7790f6ebea28b4b0c
Kevin Wessa <kjwessa@gmail.com>
2024-09-05 18:21:47 -0400

    feat(perf): utilize Next images on the About page components

--- src/app/(frontend)/(inner)/about/AboutTeam.tsx
+++ src/app/(frontend)/(inner)/about/AboutTeam.tsx
@@ -2,26 +2,15 @@ export function AboutTeam() {
   return (
     <>
       <div>
-        <div
-          className="content-stretch items-start justify-start bg-neutral-900 px-24 py-48 font-light text-white"
-          id="div-1"
-        >
-          <div
-            className="m-auto flex w-full max-w-[62.50rem] flex-grow auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto_auto] flex-col items-stretch justify-center self-start"
-            id="div-2"
-          >
-            <div
-              className="grid auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto] gap-x-[3.13rem] gap-y-[6.25rem]"
-              id="div-3"
-            >
+        <div className="content-stretch items-start justify-start bg-neutral-900 px-24 py-48 font-light text-white">
+          <div className="m-auto flex w-full max-w-[62.50rem] flex-grow auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto_auto] flex-col items-stretch justify-center self-start">
+            <div className="grid auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto] gap-x-[3.13rem] gap-y-[6.25rem]">
               <div className="flex flex-col items-center text-center">
-                <div className="h-[37.50rem] w-96 pb-8" id="div-4">
+                <div className="h-[37.50rem] w-96 pb-8">
                   <div className="overflow-hidden rounded-xl">
-                    <img
+                    <Image alt="Replace with an informative alt text"
                       className="inline-block h-[37.50rem] w-96 max-w-full object-cover align-middle"
-                      id="img-1"
-                      src="https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f8986ce296e3d5512d59_kevin-profile-pic.1920.jpg"
-                      srcSet="https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f8986ce296e3d5512d59_kevin-profile-pic.1920-p-500.jpeg 500w, https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f8986ce296e3d5512d59_kevin-profile-pic.1920.jpg 1280w"
+                      src="/5f53f8986ce296e3d5512d59_kevin-profile-pic.1920.jpg" width={1000} height={1000}
                     />
                   </div>
                 </div>
@@ -33,13 +22,11 @@ export function AboutTeam() {
                 </div>
               </div>
               <div className="flex flex-col items-center text-center">
-                <div className="h-[37.50rem] w-96 pb-8" id="div-5">
+                <div className="h-[37.50rem] w-96 pb-8">
                   <div className="overflow-hidden rounded-xl">
-                    <img
+                    <Image alt="Replace with an informative alt text"
                       className="inline-block h-[37.50rem] w-96 max-w-full object-cover align-middle"
-                      id="img-2"
-                      src="https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f783d20ccc4fe020678f_christine-profile-pic.1920.jpg"
-                      srcSet="https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f783d20ccc4fe020678f_christine-profile-pic.1920-p-500.jpeg 500w, https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f783d20ccc4fe020678f_christine-profile-pic.1920-p-800.jpeg 800w, https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f783d20ccc4fe020678f_christine-profile-pic.1920-p-1080.jpeg 1080w, https://cdn.prod.website-files.com/5dad037b65b2d9bc97118b77/5f53f783d20ccc4fe020678f_christine-profile-pic.1920.jpg 1280w"
+                      src="/5f53f783d20ccc4fe020678f_christine-profile-pic.1920.jpg" width={1000} height={1000}
                     />
                   </div>
                 </div>
