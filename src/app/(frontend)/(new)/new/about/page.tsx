1c5be2cae9442b4cd6150a7168b94694385197d6
Kevin Wessa <kjwessa@gmail.com>
2024-09-12 21:28:17 -0400

    feat(ui): add the initial version of the about page

---
+++ src/app/(frontend)/(new)/new/about/page.tsx
@@ -0,0 +1,57 @@
+export default function About() {
+  return (
+    <>
+      <section className="relative overflow-hidden bg-white pb-48 pt-24 font-medium text-black">
+        <div className="relative m-auto w-full max-w-[120.00rem] flex-col px-8">
+          <div className="relative m-auto flex max-h-[93.00rem] min-h-screen w-full max-w-[100.50rem] items-center justify-center py-96">
+            <div className="absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center">
+              <img
+                className="inline-block h-[53.13rem] w-[53.13rem] max-w-full align-middle"
+                src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefd27a2a77b43d7ab2f5_Logo%20Circle.svg"
+                alt="Logo Circle"
+              />
+            </div>
+            <div className="absolute bottom-0 left-0 right-0 top-0 z-[2]">
+              <div className="absolute bottom-auto left-0 right-auto top-[10%] h-40 w-56 overflow-hidden min-[1920px]:w-96">
+                <img
+                  className="absolute bottom-auto left-0 right-auto top-0 inline-block h-full w-full max-w-full object-cover align-middle"
+                  src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefce3986a75beb22168d_Image%2031.jpg"
+                />
+              </div>
+              <div className="absolute bottom-auto left-[49%] right-auto top-0 h-40 w-56 overflow-hidden min-[1920px]:left-[53%] min-[1920px]:w-96">
+                <img
+                  className="absolute bottom-auto left-0 right-auto top-0 inline-block h-full w-full max-w-full object-cover align-middle"
+                  src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefce4e70f0260f25587d_Image%2021.jpg"
+                />
+              </div>
+              <div className="absolute bottom-auto left-auto right-0 top-[19%] h-40 w-56 overflow-hidden min-[1920px]:top-[38%] min-[1920px]:w-96">
+                <img
+                  className="absolute bottom-auto left-0 right-auto top-0 inline-block h-full w-full max-w-full object-cover align-middle"
+                  src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefce3986a75beb2216bc_Image%2041.jpg"
+                />
+              </div>
+              <div className="absolute bottom-auto left-auto right-[17%] top-[81%] h-40 w-56 overflow-hidden min-[1920px]:right-[10%] min-[1920px]:top-[79%] min-[1920px]:w-96">
+                <img
+                  className="absolute bottom-auto left-0 right-auto top-0 inline-block h-full w-full max-w-full object-cover align-middle"
+                  src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefcf82adecd34ec59e3a_Image%2051.jpg"
+                />
+              </div>
+              <div className="absolute bottom-auto left-[17%] right-auto top-[70%] h-40 w-56 overflow-hidden min-[1920px]:left-[7%] min-[1920px]:w-96">
+                <img
+                  className="absolute bottom-auto left-0 right-auto top-0 inline-block h-full w-full max-w-full object-cover align-middle"
+                  src="https://cdn.prod.website-files.com/6495557c326b2d376ef99ea9/649aefce3986a75beb221691_Image%2061.jpg"
+                />
+              </div>
+            </div>
+            <div className="relative z-10 w-full max-w-[69.25rem] text-[6.25rem] leading-none">
+              <h2>
+                Ou<span className="text-black">r</span> mission — <br />
+                to make the internet a beautiful place.
+              </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
