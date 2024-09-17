6b7e8df2863b1e51b5d44eb8819aeb9045b84065
Kevin Wessa <kjwessa@gmail.com>
2024-09-17 17:33:11 -0400

    chore: update Next config with Cloudflare endpoint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 
+if (!process.env.CLOUDFLARE_ENDPOINT) {
+  throw new Error(
+    "CLOUDFLARE_ENDPOINT is not set in the environment variables",
+  );
+}
+
+const cloudflareHostname = new URL(process.env.CLOUDFLARE_ENDPOINT).hostname;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -8,6 +16,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
+    domains: ["localhost", cloudflareHostname],
   },
 };
 
