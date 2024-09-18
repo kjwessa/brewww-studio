2461ae1186754eab36b253bdf9ee80b5713b0e97
Kevin Wessa <kjwessa@gmail.com>
2024-09-18 14:09:59 -0400

    feat(ui): add initial blog grid structure

--- src/app/(frontend)/(inner)/blog/page.tsx
+++ src/app/(frontend)/(inner)/blog/page.tsx
@@ -1,3 +1,173 @@
 export default function BlogPage() {
-  return <div>Blog</div>;
+  return (
+    <>
+      <section className="bg-neutral-900 py-24">
+        <div className="container mx-auto">
+          <h1 className="mb-12 text-4xl font-bold text-white">Insights</h1>
+          <div
+            className="relative grid auto-cols-fr grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[auto] flex-col items-start gap-x-5 gap-y-[6.25rem] text-sm font-semibold text-zinc-100"
+            id="div-1"
+          >
+            <div className="flex h-full items-stretch">
+              <a
+                className="flex w-full max-w-full flex-col items-stretch justify-between"
+                href="#"
+              >
+                <div className="cursor-pointer">
+                  <div className="relative w-full overflow-hidden rounded bg-zinc-500/[0.2] pt-[75%]">
+                    <img
+                      className="absolute bottom-0 left-0 top-0 inline-block h-[120%] w-full max-w-full object-cover align-middle"
+                      src="https://cdn.prod.website-files.com/66b6229adea369db31b34ab3/66cb3f740b23652ceaabc838_work-thumb-111.jpg"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-[0.38rem]">
+                    <h3 className="text-xl mix-blend-difference">
+                      We're one of the 50 companies that creatives 'would kill
+                      to work for'
+                    </h3>
+                    <div className="text-zinc-500">
+                      Our long journey with Destination NSW to study travel
+                      behaviours in a pre-pandemic world.
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer">
+                  <div className="col-span-3 row-span-1 flex flex-wrap items-center justify-between gap-[0.13rem] text-xs uppercase text-zinc-500">
+                    <div className="col-span-5 row-span-1">Talks</div>
+                    <div>5 min</div>
+                  </div>
+                  <div className="my-3 h-0 w-full bg-zinc-500 opacity-25" />
+                </div>
+              </a>
+            </div>
+            <div className="flex h-full items-stretch">
+              <a
+                className="flex w-full max-w-full flex-col items-stretch justify-between"
+                href="#"
+              >
+                <div className="cursor-pointer">
+                  <div className="relative w-full overflow-hidden rounded bg-zinc-500/[0.2] pt-[75%]">
+                    <img
+                      className="absolute bottom-0 left-0 top-0 inline-block h-[120%] w-full max-w-full object-cover align-middle"
+                      src="https://cdn.prod.website-files.com/66b6229adea369db31b34ab3/66cb3fc4e3d0eaf4e5a8feec_1.jpg"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-[0.38rem]">
+                    <h3 className="text-xl mix-blend-difference">
+                      New hires: Marlina Fletcher
+                    </h3>
+                    <div className="text-zinc-500">
+                      Our long journey with Destination NSW to study travel
+                      behaviours in a pre-pandemic world.
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer">
+                  <div className="col-span-3 row-span-1 flex flex-wrap items-center justify-between gap-[0.13rem] text-xs uppercase text-zinc-500">
+                    <div className="col-span-5 row-span-1">News</div>
+                    <div>6 min</div>
+                  </div>
+                  <div className="my-3 h-0 w-full bg-zinc-500 opacity-25" />
+                </div>
+              </a>
+            </div>
+            <div className="flex h-full items-stretch">
+              <a
+                className="flex w-full max-w-full flex-col items-stretch justify-between"
+                href="#"
+              >
+                <div className="cursor-pointer">
+                  <div className="relative w-full overflow-hidden rounded bg-zinc-500/[0.2] pt-[75%]">
+                    <img
+                      className="absolute bottom-0 left-0 top-0 inline-block h-[120%] w-full max-w-full object-cover align-middle"
+                      src="https://cdn.prod.website-files.com/66b6229adea369db31b34ab3/66cb3fd69a584c42887a2cc1_1.jpg"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-[0.38rem]">
+                    <h3 className="text-xl mix-blend-difference">
+                      Another successful year at The Webbys
+                    </h3>
+                    <div className="text-zinc-500">
+                      Our long journey with Destination NSW to study travel
+                      behaviours in a pre-pandemic world.
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer">
+                  <div className="col-span-3 row-span-1 flex flex-wrap items-center justify-between gap-[0.13rem] text-xs uppercase text-zinc-500">
+                    <div className="col-span-5 row-span-1">Awards</div>
+                    <div>6 min</div>
+                  </div>
+                  <div className="my-3 h-0 w-full bg-zinc-500 opacity-25" />
+                </div>
+              </a>
+            </div>
+            <div className="flex h-full items-stretch">
+              <a
+                className="flex w-full max-w-full flex-col items-stretch justify-between"
+                href="#"
+              >
+                <div className="cursor-pointer">
+                  <div className="relative w-full overflow-hidden rounded bg-zinc-500/[0.2] pt-[75%]">
+                    <img
+                      className="absolute bottom-0 left-0 top-0 inline-block h-[120%] w-full max-w-full object-cover align-middle"
+                      src="https://cdn.prod.website-files.com/66b6229adea369db31b34ab3/66cb3fe80b23652ceaac24b4_2.jpg"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-[0.38rem]">
+                    <h3 className="text-xl mix-blend-difference">
+                      Is all that Sparkles gold?
+                    </h3>
+                    <div className="text-zinc-500">
+                      Our long journey with Destination NSW to study travel
+                      behaviours in a pre-pandemic world.
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer">
+                  <div className="col-span-3 row-span-1 flex flex-wrap items-center justify-between gap-[0.13rem] text-xs uppercase text-zinc-500">
+                    <div className="col-span-5 row-span-1">Awards</div>
+                    <div>6 min</div>
+                  </div>
+                  <div className="my-3 h-0 w-full bg-zinc-500 opacity-25" />
+                </div>
+              </a>
+            </div>
+            <div className="flex h-full items-stretch">
+              <a
+                className="flex w-full max-w-full flex-col items-stretch justify-between"
+                href="#"
+              >
+                <div className="cursor-pointer">
+                  <div className="relative w-full overflow-hidden rounded bg-zinc-500/[0.2] pt-[75%]">
+                    <img
+                      className="absolute bottom-0 left-0 top-0 inline-block h-[120%] w-full max-w-full object-cover align-middle"
+                      src="https://cdn.prod.website-files.com/66b6229adea369db31b34ab3/66cb3fffc6fa0145369b2214_framer-website-template-graphicdesign-001-3.jpg"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-[0.38rem]">
+                    <h3 className="text-xl mix-blend-difference">
+                      Why creative education for advertising is stuck in the
+                      dark ages
+                    </h3>
+                    <div className="text-zinc-500">
+                      Our long journey with Destination NSW to study travel
+                      behaviours in a pre-pandemic world.
+                    </div>
+                  </div>
+                </div>
+                <div className="cursor-pointer">
+                  <div className="col-span-3 row-span-1 flex flex-wrap items-center justify-between gap-[0.13rem] text-xs uppercase text-zinc-500">
+                    <div className="col-span-5 row-span-1">News</div>
+                    <div>5 min</div>
+                  </div>
+                  <div className="my-3 h-0 w-full bg-zinc-500 opacity-25" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
