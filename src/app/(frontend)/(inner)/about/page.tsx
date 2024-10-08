0f3b2fe72e66e2e552e87b6576211ed33dddad1d
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-10-08 08:26:14 -0400

    feat(ui): add new sections explaining the studio on the about page

--- src/app/(frontend)/(inner)/about/page.tsx
+++ src/app/(frontend)/(inner)/about/page.tsx
@@ -1271,6 +1271,95 @@ export default function About() {
           </div>
         </div>
       </section>
+
+      <section className="flex min-h-[80vh] flex-col justify-between bg-white p-9 text-[2.75rem] leading-none text-stone-950">
+        <div className="absolute bottom-[830.13rem] left-0 right-0 top-[-3.13rem] z-[-1] bg-[linear-gradient(198.91deg,_rgb(255,_255,_255)_64.12%,_rgb(250,_246,_249)_72.26%,_rgb(251,_234,_248)_81.42%,_rgb(252,_213,_245)_89.56%,_rgb(254,_185,_241)_98.72%,_rgb(255,_173,_239)_101.78%)]" />
+        <div className="text-7xl">
+          <h1 className="max-w-4xl">
+            It's our pursuit to eradicate mediocrity from this world. And that
+            makes 'becoming obsolete' our ultimate goal.
+          </h1>
+        </div>
+        <div className="flex items-end justify-between text-[1.38rem] leading-7">
+          <div>
+            <p className="text-xl">
+              <strong className="text-neutral-400">Latest insight: </strong>
+              <em className="cursor-pointer italic">
+                Elevating brands: Strategies for market leadership
+              </em>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative h-[75vh] overflow-hidden text-stone-950">
+        <div className="absolute inset-0">
+          <picture>
+            <source srcSet="https://www.datocms-assets.com/63464/1661346969-stuurmen-office-interior.jpg?auto=format&dpr=0.25&w=2200 550w,https://www.datocms-assets.com/63464/1661346969-stuurmen-office-interior.jpg?auto=format&dpr=0.5&w=2200 1100w,https://www.datocms-assets.com/63464/1661346969-stuurmen-office-interior.jpg?auto=format&dpr=0.75&w=2200 1650w,https://www.datocms-assets.com/63464/1661346969-stuurmen-office-interior.jpg?auto=format&w=2200 2200w" />
+            <img
+              className="h-full w-full object-cover"
+              src="https://www.datocms-assets.com/63464/1661346969-stuurmen-office-interior.jpg?auto=format&w=2200"
+              alt="Stuurmen office interior"
+            />
+          </picture>
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h2 className="text-8xl font-bold uppercase text-white">
+            Kill off the Average
+          </h2>
+        </div>
+      </section>
+
+      <section className="bg-white py-24 text-[1.38rem] leading-7 text-stone-950">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-between">
+            <div className="mb-16 w-full">
+              <h2 className="max-w-3xl text-7xl">
+                Mediocrity does not stand a chance with us
+              </h2>
+            </div>
+            <div className="mt-8 w-full lg:w-1/2 lg:pr-16">
+              <div className="mb-12 text-[1.63rem] leading-8">
+                <p className="text-xl">
+                  Customers come and go. Many products barely meet expectations.
+                  Most brand promises are never kept. Mediocrity is everywhere
+                  and fatal to business success. But industry leaders are never
+                  mediocre. Never.
+                  <br />
+                  <br />
+                  We set high standards for ourselves and our clients. We are
+                  driven by the need to unleash full potential and get the most
+                  out of every situation. With top-notch branding, we do justice
+                  to our mantra: 'Kill off the average™'. Mediocrity is way too
+                  boring.
+                </p>
+              </div>
+              <div className="mt-16 text-xl">
+                <strong className="font-bold">The label premium</strong>
+                <p className="mt-4">
+                  We work exclusively with companies that are keen to become or
+                  remain a leader within their industry. Companies that dare to
+                  swim against the current. Companies that are perfectly aware
+                  of the fact that the bar can always be set higher, and so are
+                  willing to do whatever it takes to take their business to the
+                  next level. In short: companies that are worthy of the label
+                  premium.
+                </p>
+              </div>
+            </div>
+            <div className="relative mt-8 w-full lg:mt-0 lg:w-1/2 lg:pl-16">
+              <div className="aspect-w-16 aspect-h-9" style={{ height: "70%" }}>
+                <img
+                  className="h-full w-full object-cover"
+                  src="https://www.datocms-assets.com/63464/1661347408-stuurmen-office-interior.jpg?auto=format&h=1080&w=1920"
+                  alt="Stuurmen office interior"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="py-24 text-[1.38rem] leading-7 text-white">
         <div className="container mx-auto">
           <div className="flex">
