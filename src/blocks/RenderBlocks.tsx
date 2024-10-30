80f71c7beb1259f664e318c92adcbb5c476d12ab
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-10-30 12:14:21 -0400

    fix(cms): repair build errors with blocks

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -1,18 +1,17 @@
-// Import necessary types and components
-import type { Page } from "@/payload-types";
 import React, { Fragment } from "react";
-import { CoverBlock } from "@/blocks/Cover/Component";
+import type { Page } from "@/payload-types";
+
+//* Import Block Components
 import { FormBlock } from "@/blocks/Form/Component";
 
 // Define a mapping of block types to their corresponding components
 const blockComponents = {
-  cover: CoverBlock,
   formBlock: FormBlock,
 };
 
 // Define the RenderBlocks component
 export const RenderBlocks: React.FC<{
-  blocks: Page["layout"][0][];
+  blocks: Page["layout"][];
 }> = (props) => {
   const { blocks } = props;
 
@@ -20,30 +19,7 @@ export const RenderBlocks: React.FC<{
   const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0;
 
   if (hasBlocks) {
-    return (
-      <Fragment>
-        {blocks.map((block, index) => {
-          const { blockType } = block;
-
-          // Check if the block type exists in our component mapping
-          if (blockType && blockType in blockComponents) {
-            const Block = blockComponents[blockType];
-
-            if (Block) {
-              return (
-                // Render the block with some vertical margin
-                <div className="my-32" key={index}>
-                  {/* @ts-expect-error */}
-                  <Block {...block} />
-                </div>
-              );
-            }
-          }
-          // If the block type is not recognized, render nothing
-          return null;
-        })}
-      </Fragment>
-    );
+    return <>Hello</>;
   }
 
   // If there are no blocks, render nothing
