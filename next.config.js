bbf5222853a49049a4522c5f3dffa49f86ecf58e
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-11-21 18:24:18 -0500

    chore: try relative paths for form checkbox to see if vercel fails

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-import { withPayload } from "@payloadcms/next/withPayload";
-import { withSentryConfig } from "@sentry/nextjs";
+import { withPayload } from '@payloadcms/next/withPayload'
+import { withSentryConfig } from '@sentry/nextjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,69 +8,69 @@ const nextConfig = {
   },
   reactStrictMode: true,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "bucket.brewww.studio",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'bucket.brewww.studio',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "cdn.prod.website-files.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cdn.prod.website-files.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "assets.website-files.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'assets.website-files.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "grainandmortar.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'grainandmortar.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "framerusercontent.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'framerusercontent.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "www.datocms-assets.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'www.datocms-assets.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "made-byshape.transforms.svdcdn.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'made-byshape.transforms.svdcdn.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
-};
+}
 
 const sentryConfig = {
-  org: "brewww-studio",
-  project: "brewww-payload",
+  org: 'brewww-studio',
+  project: 'brewww-payload',
   silent: !process.env.CI,
   widenClientFileUpload: true,
   reactComponentAnnotation: {
     enabled: true,
   },
-  tunnelRoute: "/monitoring",
+  tunnelRoute: '/monitoring',
   hideSourceMaps: true,
   disableLogger: true,
   automaticVercelMonitors: true,
-};
+}
 
 // Apply both Payload and Sentry configurations
-const config = withSentryConfig(withPayload(nextConfig), sentryConfig);
+const config = withSentryConfig(withPayload(nextConfig), sentryConfig)
 
-export default config;
+export default config
