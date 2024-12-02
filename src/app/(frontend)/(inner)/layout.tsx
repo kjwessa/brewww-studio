5df6e549ce1d7f1ee9cd2c825a6d3cc75e3109ef
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-12-02 11:26:37 -0500

    feat(nav): add header and footer to site

--- src/app/(frontend)/(inner)/layout.tsx
+++ src/app/(frontend)/(inner)/layout.tsx
@@ -1,73 +1,63 @@
-import localFont from "next/font/local";
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import Header from "@/components/Header/index";
-import Footer from "@/components/Footer/index";
-import { LivePreviewListener } from "@/components/LivePreviewListener";
-import { GridGuide } from "@/components/GridGuide/index";
-import { Grain } from "@/components/Grain/index";
-import { AdminBar } from "@/components/AdminBar";
-import { mergeOpenGraph } from "@/utilities/mergeOpenGraph";
-import { draftMode } from "next/headers";
+import localFont from 'next/font/local'
+import '@/styles/globals.css'
+import { Metadata } from 'next'
+import Header from '@/components/Header/index'
+import Footer from '@/components/Footer/index'
+import { LivePreviewListener } from '@/components/LivePreviewListener'
+import { GridGuide } from '@/components/GridGuide/index'
+import { Grain } from '@/components/Grain/index'
+import { AdminBar } from '@/components/AdminBar'
+import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
+import { draftMode } from 'next/headers'
 
 const DMSans = localFont({
-  variable: "--font-dm-sans",
-  display: "swap",
+  variable: '--font-dm-sans',
+  display: 'swap',
   src: [
     {
-      path: "../../fonts/DMSans.ttf",
-      weight: "100 400 900",
-      style: "normal",
+      path: '../../fonts/DMSans.ttf',
+      weight: '100 400 900',
+      style: 'normal',
     },
   ],
-});
+})
 
 const BebasNeue = localFont({
-  variable: "--font-bebas-neue",
-  display: "swap",
+  variable: '--font-bebas-neue',
+  display: 'swap',
   src: [
     {
-      path: "../../fonts/BebasNeue.ttf",
-      weight: "400",
-      style: "normal",
+      path: '../../fonts/BebasNeue.ttf',
+      weight: '400',
+      style: 'normal',
     },
   ],
-});
+})
 
-export default async function InnerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const { isEnabled } = await draftMode();
+export default async function InnerLayout({ children }: { children: React.ReactNode }) {
+  const { isEnabled } = await draftMode()
 
   return (
-    <html
-      lang="en"
-      className={`${DMSans.variable} ${BebasNeue.variable}`}
-      suppressHydrationWarning
-    >
+    <html lang="en" className={`${DMSans.variable} ${BebasNeue.variable}`} suppressHydrationWarning>
       <body className="bg-gray-950 text-gray-50 antialiased">
         <AdminBar adminBarProps={{ preview: isEnabled }} />
         <Grain>
           <LivePreviewListener />
-          {/* <Header /> */}
+          <Header />
           <main className="min-h-[80vh]">{children}</main>
-          {/* <Footer /> */}
+          <Footer />
         </Grain>
         <GridGuide />
       </body>
     </html>
-  );
+  )
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SERVER_URL || "https://brewww.studio",
-  ),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL || 'https://brewww.studio'),
   openGraph: mergeOpenGraph(),
   twitter: {
-    card: "summary_large_image",
-    creator: "@brewwwstudio",
+    card: 'summary_large_image',
+    creator: '@brewwwstudio',
   },
-};
+}
