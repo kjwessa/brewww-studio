9815add77d1c902818465ac2169b6329583432d1
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-12-03 13:53:23 -0500

    chore(robots): remove blocks to google bots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,56 +1,17 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next'
 
-const serverUrl = process.env.SITE_URL;
+const serverUrl = process.env.SITE_URL
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/api",
-          "/admin",
-          "/private",
-          "/_next",
-          "/studio",
-          "/cdn-cgi",
-        ],
-      },
-      {
-        userAgent: "GPTBot",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "Claude-Web",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "Google-Extended",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "CCBot",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "anthropic-ai",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "Omgilibot",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "Omgili",
-        disallow: ["/"],
-      },
-      {
-        userAgent: "FacebookBot",
-        disallow: ["/"],
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api', '/admin', '/private', '/_next', '/cdn-cgi'],
       },
     ],
     sitemap: `${serverUrl}/sitemap.xml`,
     host: serverUrl,
-  };
+  }
 }
