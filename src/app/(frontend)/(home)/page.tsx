8cabc3334228a1db5b3f80162ac33cbb6a9077c3
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-12-09 13:31:19 -0500

    fix(home): update logo link

--- src/app/(frontend)/(home)/page.tsx
+++ src/app/(frontend)/(home)/page.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 export default function Page() {
   return (
-    <div
-      className="fixed z-50 flex min-h-dvh w-full items-center justify-center bg-brand-dark-bg"
-    >
+    <div className="bg-brand-dark-bg fixed z-50 flex min-h-dvh w-full items-center justify-center">
       <div className="container mx-auto flex flex-col items-center gap-4">
         <Image
-          src="/images/brand/brewww-mark-gold.png"
+          src="https://bucket.brewww.studio/brewww/media/brewww_logo_mark_letter-b_gold.svg"
           width={256}
           height={256}
           alt="Brewww Studio logo"
